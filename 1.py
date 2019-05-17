7212ecb1c837cf803193b7f61976da9b041aff73
parmis <parmis_sp1996@yahoo.com>
2019-05-17 10:24:12 +0330

    12

--- 1.py
+++ 1.py
@@ -6,3 +6,16 @@ num = 31
 
 test =(a < num < b) or (c < num < d)
 print(test)
+
+#----------
+weight = 60
+height = 180
+BMI = weight / (height * height)
+if  BMI < 18 :
+    print("under weight")
+
+
+if  18 < BMI < 25 :
+        print("Normal")
+if BMI>25 :
+    print("Overweight")
