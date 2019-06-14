f05c84da68d8d88e45e14df0b137d5882b811048
parmis3 <parmis_sp1996@yahoo.com>
2019-06-14 10:20:11 +0330

    1234

---
+++ s04/txt.py
@@ -0,0 +1,13 @@
+def unique_words(text):
+    words = text.split()
+
+    return words
+
+
+text = " hello hello hi hello hi hi hi "
+res = unique_words(text)
+print(res)
+
+textt="salam salam chetori chetori hi hi hello"
+ress= unique_words(textt)
+print(ress)
