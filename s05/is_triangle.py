0c4dc21694302cd8a1890fd64e29e78b8f9a3c30
parmis3 <parmis_sp1996@yahoo.com>
2019-06-21 12:37:34 +0330

    111

---
+++ s05/is_triangle.py
@@ -0,0 +1,10 @@
+def is_triagle (a,b,c):
+
+
+    if a<b+c  and b<a+c  and c<a+b:
+        return True
+
+
+    return False
+
+f = open('input.txt')
---
+++ s05/is_triangle2.py
@@ -0,0 +1,7 @@
+def is_tirangle(a,b,c):
+    if a==b==c:
+        print("moslas Motesaviolazla ast")
+    elif a==b or a==c or b==c:
+        print("mosalas Motesaviolazla ast")
+    else:
+        print("mosalas noe digari ast")
